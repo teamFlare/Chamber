e74212a02b881b0af99d4211a7cb01121940e25d
Nikhil Mehta <nikhilrmehta7@gmail.com>
2017-05-31 15:27:49 -0700

    (feat) return top beats and top collaborations when requested by the server

--- db/index.js
+++ db/index.js
@@ -3,5 +3,29 @@ const db = require('bookshelf')(knex);
 
 db.plugin('registry');
 
+var beatsQuery = knex.table('likes').innerJoin('submissions', 'likes.submission_id', '=', 'submissions.id').where({'submissions.type':'beat'}).as('beatsTable');
+var orderedQuery = knex.select('submission_id').count('submission_id').from(beatsQuery).groupBy('submission_id').orderByRaw('count(distinct submission_id) desc limit 5').as('orderedTable');
+
+knex(orderedQuery).innerJoin('submissions', 'orderedTable.submission_id', '=', 'submissions.id')
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+var collabsQuery = knex.table('likes').innerJoin('collaborations', 'likes.collaboration_id', '=', 'collaborations.id').as('collabsTable');
+var orderedQuery = knex.select('collaboration_id').count('collaboration_id').from(collabsQuery).groupBy('collaboration_id').orderByRaw('count(distinct collaboration_id) desc limit 5').as('orderedTable');
+
+knex(orderedQuery).innerJoin('collaborations', 'orderedTable.collaboration_id', '=', 'collaborations.id')
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+
 module.exports = db;
 
