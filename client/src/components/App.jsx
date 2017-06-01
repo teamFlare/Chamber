8ef91ae666e71ff4ba471710aef418280eb098eb
Steve <stevenjchang@gmail.com>
2017-06-01 16:26:29 -0700

    Create components/App

---
+++ client/src/components/App.jsx
@@ -0,0 +1,27 @@
+import  { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreators';
+import Homepage from './Homepage';
+
+//mapStateToProps - will take the state
+function mapStateToProps(state) {
+  //return the state that you need
+  return {
+    posts: state.posts,
+    comments: state.comments
+  }
+}
+
+// dispatch = function creators
+// it will surface the data and functions via props
+// mapDispatchToProps will eliminate the need to manually run $r.store.dispatch
+function mapDispatchToProps(dispatch) {
+  // will pass action creators via props
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Homepage);
+// all of our data and action creators are available inside Homepage
+// connect will inject the data from our store to whatever level component that we actually need
+
+export default App;
