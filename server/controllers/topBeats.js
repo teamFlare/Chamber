bbc135eb4cfe7b2f39f1998e3790e50992970eed
Steve <stevenjchang@gmail.com>
2017-06-02 16:00:20 -0700

    Implement getSongs inside controllers/topBeats

--- server/controllers/topBeats.js
+++ server/controllers/topBeats.js
@@ -1,12 +1,21 @@
 const models = require('../../db/models');
+const knex = require('knex')(require('../../knexfile'));
+const db = require('../../db/index');
 
-module.exports.getAll = (req, res) => {
-  models.Profile.fetchAll()
-    .then(profiles => {
-      res.status(200).send(profiles);
-    })
-    .catch(err => {
-      // This code indicates an outside service (the database) did not respond in time
-      res.status(503).send(err);
-    });
+module.exports.getSongs = (req, res) => {
+  db.getSongsFromDb(function(response, error) {
+    if (error) {
+      console.log('Error! getSongs inside controllers/topBeats', error);
+    } else {
+      res.status(200).send(response);
+    }
+  })
+  // models.Profile.fetchAll()
+  //   .then(songs => {
+  //     res.status(200).send(songs);
+  //   })
+  //   .catch(err => {
+  //     // This code indicates an outside service (the database) did not respond in time
+  //     res.status(503).send(err);
+  //   });
 };
