1bdbd6ab4dde9ae68dabc656beff49e094f773f5
Steve <stevenjchang@gmail.com>
2017-06-02 10:51:20 -0700

    Add topBeats routes and controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,1 +1,2 @@
 module.exports.Profiles = require('./profiles');
+module.exports.TopBeats = require('./topBeats');
---
+++ server/controllers/topBeats.js
@@ -0,0 +1,12 @@
+const models = require('../../db/models');
+
+module.exports.getAll = (req, res) => {
+  models.Profile.fetchAll()
+    .then(profiles => {
+      res.status(200).send(profiles);
+    })
+    .catch(err => {
+      // This code indicates an outside service (the database) did not respond in time
+      res.status(503).send(err);
+    });
+};
