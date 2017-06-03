9ab73f8bb6bfc803794908bfd77792e4c4bd83a1
Steve <stevenjchang@gmail.com>
2017-06-03 10:51:57 -0700

    Implement commentRender

---
+++ server/controllers/commentRender.js
@@ -0,0 +1,14 @@
+const models = require('../../db/models');
+const knex = require('knex')(require('../../knexfile'));
+const db = require('../../db/index');
+
+module.exports.commentRender = (req, res) => {
+  console.log('************',req.query.collab_id)
+  db.getCommentsFromDb(function(response, error) {
+    if (error) {
+      console.log('Error! getSongs inside controllers/topBeats', error);
+    } else {
+      res.status(200).send(response);
+    }
+  }, req.query.collab_id)
+};
