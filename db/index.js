511ce725b65825a5c76e6b3d0c67cd9f34cebe87
Steve <stevenjchang@gmail.com>
2017-06-03 11:15:12 -0700

    Cleanup topBeats function

--- db/index.js
+++ db/index.js
@@ -6,37 +6,6 @@ db.plugin('registry');
 function getSongsFromDb(callback) {
 var beatsQuery = knex.table('likes').innerJoin('submissions', 'likes.submission_id', '=', 'submissions.id').where({'submissions.type':'beat'}).as('beatsTable');
 var orderedQuery = knex.select('submission_id').count('submission_id').from(beatsQuery).groupBy('submission_id').orderByRaw('count(distinct submission_id) desc limit 5').as('orderedTable');
-<<<<<<< HEAD
-var topBeats = knex(orderedQuery).innerJoin('submissions', 'orderedTable.submission_id', '=', 'submissions.id').as('topbeatTable');
-
-knex(topBeats).innerJoin('profiles', 'topbeatTable.profiles_id', '=', 'profiles.id')
-    .then((response) => {
-        // console.log(response)
-        callback(response, null)
-    })
-    .catch((error) => {
-        // console.log(error)
-        callback(null, error)
-    })
- }
-
-// function getSongsFromDb(callback) {
-//   var collabsQuery = knex.table('likes').innerJoin('collaborations', 'likes.collaboration_id', '=', 'collaborations.id').as('collabsTable');
-//   var orderedQuery = knex.select('collaboration_id').count('collaboration_id').from(collabsQuery).groupBy('collaboration_id').orderByRaw('count(distinct collaboration_id) desc limit 5').as('orderedTable');
-//   var topCollabs = knex(orderedQuery).innerJoin('collaborations', 'orderedTable.collaboration_id', '=', 'collaborations.id').as('topcollabTable');
-//   var beatid = knex(topCollabs).innerJoin('submissions', 'topcollabTable.beat_id', '=', 'submissions.id').as('beatidTable');
-//   var beatProfile = knex(beatid).join('profiles', 'beatidTable.profiles_id', '=', 'profiles.id').select('*','beatidTable.profiles_id as beatProfId','beatidTable.tempo as beatTempo', 'profiles.display as beatDisplay', 'profiles.first as beatFirst', 'profiles.last as beatLast').as('beatprofileTable');
-//   var collabid = knex(beatProfile).innerJoin('submissions', 'beatprofileTable.collab_id', '=', 'submissions.id').select('*', 'submissions.tempo as vocalTempo', 'submissions.profiles_id as vocalId').as('collabidTable');
-
-//   knex(collabid).join('profiles', 'collabidTable.vocalId', '=', 'profiles.id').select('*', 'profiles.display as vocalDisplay', 'profiles.first as vocalFirst', 'profiles.last as vocalLast').orderBy('count', 'desc')
-//       .then((response) => {
-//           callback(response, null)
-//       })
-//       .catch((error) => {
-//           callback(null, error)
-//       })
-// }
-=======
 var topBeats = knex(orderedQuery).innerJoin('submissions', 'orderedTable.submission_id', '=', 'submissions.id').orderBy('count').as('topbeatTable');
 
 knex(topBeats).innerJoin('profiles', 'topbeatTable.profiles_id', '=', 'profiles.id')
@@ -78,7 +47,6 @@ function getCommentsFromDb(callback, collab_id) {
           callback(null, error)
       })
 }
->>>>>>> Implement getSongsFromDb(version 2), getCommentsFromDb, postVoteToDb
 
 function postVoteToDb(voteObject, callback) {
   knex('likes').insert({profiles_id: voteObject.profile_id, submission_id: voteObject.collabs_id})
