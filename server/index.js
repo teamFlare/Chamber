8c27caf245c00285da104c15f22b9047983044e8
Nikhil Mehta <NikhilMehta@ARJUNMACBOOK.local>
2017-06-15 13:32:55 -0700

    changed port

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const app = require('./app');
 const db = require('../db');
-const PORT = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log('Example app listening on port 8080!');
 });
