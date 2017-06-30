e0b6df1cd7eff133d55d76314610c66ba139f83b
Steve <stevenjchang@gmail.com>
2017-06-29 18:38:08 -0700

    heroku deploy tweak: remove LOCAL_PG again

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
-const LOCAL_PG = {
-    "database": "thesis",
-    "user": "postgres",
-    "password": "postgres",
-    "host": "localhost",
-    "port": 5432
-}
+// const LOCAL_PG = {
+//     "database": "thesis",
+//     "user": "postgres",
+//     "password": "postgres",
+//     "host": "localhost",
+//     "port": 5432
+// }
 //deploy heroku
 module.exports = {
     client: "postgresql",
-    connection: LOCAL_PG || process.env.DATABASE_URL + '? ssl=true',
+    connection: process.env.DATABASE_URL + '? ssl=true',
     pool: {
         "min": 2,
         "max": 10000
